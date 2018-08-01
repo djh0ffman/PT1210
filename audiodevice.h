d4dd5b7f06574d4f5969999fc49d9ec7d0acddb8
Dale Whinham <daleyo@gmail.com>
2018-08-01 16:51:55 +0100

    Audio: Add missing header guards

--- audiodevice.h
+++ audiodevice.h
@@ -11,7 +11,12 @@
  * Audio device allocation/release.
  */
 
+#ifndef AUDIO_DEVICE_H
+#define AUDIO_DEVICE_H
+
 #include <stdbool.h>
 
 bool pt1210_audio_open_device();
 void pt1210_audio_close_device();
+
+#endif /* AUDIO_DEVICE_H */
