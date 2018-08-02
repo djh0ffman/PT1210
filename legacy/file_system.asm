f6148015b25034bbb3420eb198ad1eddcdf15b62
Unknown <ianhoffmanuk@gmail.com>
2018-08-02 22:18:42 +0100

    tidy up tiny file
    not much left now :D

--- legacy/file_system.asm
+++ legacy/file_system.asm
@@ -1,7 +1,5 @@
 
 	include exec/memory.i
-	include dos/dos.i
-	include dos/dos_lib.i
 	
 ExecBase	= 4
 AllocMem	= -$c6
@@ -56,36 +54,34 @@ mi_SortBPMDesc
 			bsr		_pt1210_file_sort_bpm_desc
 			rts
 
-freechip
-	movem.l	d1-a6,-(sp)
-	move.l	#MEMF_CHIP|MEMF_LARGEST,d1
-	move.l	ExecBase,a6
-	jsr	AvailMem(a6)
-	movem.l	(sp)+,d1-a6
-	rts
+freechip	movem.l	d1-a6,-(sp)
+			move.l	#MEMF_CHIP|MEMF_LARGEST,d1
+			move.l	ExecBase,a6
+			jsr	AvailMem(a6)
+			movem.l	(sp)+,d1-a6
+			rts
 
 
-allocchip
-	movem.l	d0-a6,-(sp)
-	move.l	memsize,d0
-	move.w	#MEMF_CHIP,d1
-	move.l	ExecBase,a6
-	jsr	AllocMem(a6)
-	move.l	d0,memptr
-	movem.l	(sp)+,d0-a6
-	rts
+allocchip	movem.l	d0-a6,-(sp)
+			move.l	memsize,d0
+			move.w	#MEMF_CHIP,d1
+			move.l	ExecBase,a6
+			jsr		AllocMem(a6)
+			move.l	d0,memptr
+			movem.l	(sp)+,d0-a6
+			rts
 
 unallocchip
-	movem.l	d0-a6,-(sp)
-	move.l	memsize,d0
-	move.l	memptr,a1
-	beq.b	.noal
-	move.l	ExecBase,a6
-	jsr	FreeMem(a6)
-	clr.l	memptr
-	clr.l	memsize
-.noal	movem.l	(sp)+,d0-a6
-	rts
+			movem.l	d0-a6,-(sp)
+			move.l	memsize,d0
+			move.l	memptr,a1
+			beq.b	.noal
+			move.l	ExecBase,a6
+			jsr		FreeMem(a6)
+			clr.l	memptr
+			clr.l	memsize
+.noal		movem.l	(sp)+,d0-a6
+			rts
 	
 memptr	dc.l	0
 memsize	dc.l	0
