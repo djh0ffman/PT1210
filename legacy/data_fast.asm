eafdcf318ccfd9414e4979e22de73e4d7383525a
Dale Whinham <daleyo@gmail.com>
2019-07-10 03:20:23 +0100

    Integrate graphics conversion into CMake
    In this commit we remove the .bin images and instead generate them at
    build time using iff2raw.
    
    The gfx folder now just contains the input IFF files, and the outputs
    are generated underneath the current build folder.

--- legacy/data_fast.asm
+++ legacy/data_fast.asm
@@ -3,8 +3,8 @@
 
 				section gfxdata_cpu,data
 
-_font_big		incbin	"../gfx/raw/font-big.raw"
-_font_small		incbin	"../gfx/raw/font-small.raw"
+_font_big		incbin	"gfx/font-big.raw"
+_font_small		incbin	"gfx/font-small.raw"
 _font_digi2		incbin	"gfx/font-digi3.raw"
 _kb				incbin	"gfx/kb.raw"
 _bpm			incbin	"gfx/bpm.raw"
